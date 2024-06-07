604757907eaedff345c4c220da55722e01bfcb08
Outfled <55547083+Outfled@users.noreply.github.com>
2024-06-07 12:02:51 -0400

    Update NTFSVolumeSearcher.h

--- NTFSFileSearch/NTFSVolumeSearcher.h
+++ NTFSFileSearch/NTFSVolumeSearcher.h
@@ -1,16 +1,17 @@
 #pragma once
 
-/* Search for files */
-#define FILE_SEARCH_FLAG_FIND_FILES			0x00000001
 
-/* Search for directories */
-#define FILE_SEARCH_FLAG_FIND_DIRECTORIES	0x00000002
+//
+// CNTFSVolumeSearcher::SetFlags() Flags
+#define FILE_SEARCH_FLAG_FIND_FILES			0x00000001 /* Include file results in the volume search (Default) */
+#define FILE_SEARCH_FLAG_FIND_DIRECTORIES	0x00000002 /* Include directory results in the volume search */
+//#define FILE_SEARCH_FLAG_CACHE_RESULTS	0x00000004 
 
 /* Search for files & directories */
 #define FILE_SEARCH_FLAG_FIND_ALL			FILE_SEARCH_FLAG_FIND_FILES | FILE_SEARCH_FLAG_FIND_DIRECTORIES
 
 
-class NTFSVolumeSearcher
+class CNTFSVolumeSearcher
 {
 private:
 	/* Basic File Search Filter */
@@ -26,19 +27,18 @@ private:
 	};
 
 	CNTFSVolume					*m_pVolume;
+	DWORD						m_dwFlags;
 	std::vector<FileFilterData>	m_rgFilters;
 	CMemoryPool					m_FileNameMemoryPool;
 	UINT64						m_nFileNameOffset;
 	MFTDataRunList				m_rgDataRuns;
-	NTFSDirectoryMap			m_DirectoryMap;
+	NTFSFileMap					m_DirectoryMap;
 	NTFSFileMap					m_FileMap;
 
 public:
-	CMFTReader();
+	CNTFSVolumeSearcher();
 
-	~CMFTReader();
-
-	BOOL SetVolume(CNTFSVolume *pVolume);
+	~CNTFSVolumeSearcher();
 
 	BOOL AddFileFilter(NTFS_FILTER_OPERATOR fOperator, NTFS_FILTER_FACTOR iFactorType, LPCWSTR lpszFilterString);
 
@@ -60,6 +60,10 @@ public:
 
 	VOID FreeFileEntries(PNTFS_FILE_ENTRYW pFileEntries);
 
+	BOOL SetFlags(DWORD dwFlags);
+
+	BOOL SetVolume(CNTFSVolume *pVolume);
+
 private:
 	VOID ParseRecordChunk(DWORD dwFindFlags, PBYTE pbRecordChunk, UINT64 cbRecordChunk);
 
@@ -69,9 +73,7 @@ private:
 	
 	BOOL CheckFileCriteria(PNTFS_FILE_ENTRYW pEntry, BOOL bFinalPath);
 
-	VOID UpdateFileRecordMapDirectoryNames(NTFSDirectoryMap &mpDirectoryMap);
+	VOID UpdateFileRecordMapDirectoryNames(NTFSFileMap &mpDirectoryMap);
 
-	VOID UpdateFileRecordMapFileNames(NTFSFileMap &mpFileMap, NTFSDirectoryMap mpDirectoryMap);
-};
-
+	VOID UpdateFileRecordMapFileNames(NTFSFileMap &mpFileMap, NTFSFileMap mpDirectoryMap);
 };
